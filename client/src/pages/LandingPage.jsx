d41ebcc2666f21c71eb41f427621129aa4c45071
Celaerius <131456370+Celaerius@users.noreply.github.com>
2026-02-18 14:18:08 +0100

    CSS
    Avencement dans le CSS de la landing Page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,16 +1,21 @@
-import AuthLayoutForm from '../components/auth/AuthLayoutForm';
+import AuthLayoutForm from "../components/auth/AuthLayoutForm";
 
 export default function Landing() {
     return (
-        <div className="landing-page">
-            <div>
-            <h1 className="landing-title">Find My Roof</h1>
-            <p className="landing-description">Tinder pour logements : Trouvez votre prochain chez-vous en un swipe !</p>
-            <button className="landing-button">Get Started</button>
-            </div>
-            <div>
-                <AuthLayoutForm />
+    <div className="landing-page">
+        <div className="landing-intro">
+            <div className="intro-container">
+                <h1 className="landing-title">Find My Roof</h1>
+                <p className="landing-description">
+                Tinder pour logements : Trouvez votre prochain chez-vous en un swipe
+                !
+                </p>
+                <button className="landing-button">Get Started</button>
             </div>
         </div>
+        <div className="landing-forms">
+            <AuthLayoutForm />
+        </div>
+    </div>
     );
 }
