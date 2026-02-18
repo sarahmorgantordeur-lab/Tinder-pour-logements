f004b6461f746086cdadacbe07e15b5804f66031
Celaerius <131456370+Celaerius@users.noreply.github.com>
2026-02-18 13:07:50 +0100

    App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import AppRouter from "./routes/AppRouter";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
     <>
-      <AppRouter />
+      <AuthProvider>
+        <AppRouter />
+      </AuthProvider>
     </>
   );
 }
