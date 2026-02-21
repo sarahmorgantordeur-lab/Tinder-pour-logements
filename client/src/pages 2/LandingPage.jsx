6fbf9916edfde090c742bad9021530e2d4da877a
Celaerius <131456370+Celaerius@users.noreply.github.com>
2026-02-21 15:01:36 +0100

    Avancement

---
+++ client/src/pages 2/LandingPage.jsx
@@ -0,0 +1,17 @@
+
+
+export default function Landing() {
+    return (
+        <div className="landing-page">
+            <div>
+            <h1 className="landing-title">Find My Roof</h1>
+            <p className="landing-description">Tinder pour logements : Trouvez votre prochain chez-vous en un swipe !</p>
+            </div>
+            <div>
+                <div>
+                    <h2 className="landing-subtitle">Login/Register</h2>
+                </div>
+            </div>
+        </div>
+    );
+}
