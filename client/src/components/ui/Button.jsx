b3890222aa1cbe3a285d923a66e8616ee3136f91
sarahmorgantordeur-lab <sarahmorgantordeur@gmail.com>
2026-03-09 16:28:14 +0100

    maj cypress

--- client/src/components/ui/Button.jsx
+++ client/src/components/ui/Button.jsx
@@ -1,8 +1,8 @@
 
 
-export default function Button({ children, onClick, className }) {
+export default function Button({ children, onClick, className, ...rest }) {
     return (
-        <button onClick={onClick} className={`btn ${className}`}>
+        <button onClick={onClick} className={`btn ${className}`} {...rest}>
             {children}
         </button>
     );
